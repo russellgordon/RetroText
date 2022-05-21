1edded40748ee772406326c7f25efd85bca60ecb
Russell Gordon <github@russellgordon.ca>
2022-05-21 07:53:55 -0400

    Added type effect modifier.

---
+++ Sources/RetroText/Enumerations/RetroTextTypeEffectSpeed.swift
@@ -0,0 +1,15 @@
+//
+//  RetroTextTypeEffectSpeed.swift
+//
+//
+//  Created by Russell Gordon on 2022-05-21.
+//
+
+import Foundation
+
+// Control typing speed
+public enum RetroTextTypeEffectSpeed: Int {
+    case fast = 1
+    case normal = 2
+    case slow = 3
+}
