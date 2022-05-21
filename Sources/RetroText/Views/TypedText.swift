b7bdaea47f776200481766bd9c690c19caa05814
Russell Gordon <github@russellgordon.ca>
2022-05-21 11:26:16 -0400

    Attempt to fix code example in documentation.

--- Sources/RetroText/Views/TypedText.swift
+++ Sources/RetroText/Views/TypedText.swift
@@ -9,6 +9,14 @@ import Foundation
 import SwiftUI
 
 /// Creates a "typed on" effect where each letter of the message is revealed over time.
+/// You can use `#` signs to introduce a pause as letters are typed, which can build tension or create drama.
+/// For example:
+///     struct ContentView: View {
+///         var body: some View {
+///            TypedText("Come with me###, and see what has been foretold#.#.#.")
+///            .padding()
+///         }
+///     }
 public struct TypedText: View {
     
     // MARK: Stored properties
@@ -91,18 +99,6 @@ public struct TypedText: View {
 
     // MARK: Functions
     /// Creates a "typed on" effect where each letter of the message is revealed over time, as controlled by the given message, speed, and debug arguments.
-    /// You can use `#` signs to introduce a pause as letters are typed, which can build tension or create drama.
-    /// For example:
-    /// ```swift
-    /// struct ContentView: View {
-    ///         var body: some View {
-    ///
-    ///             TypedText("Come with me###, and see what has been foretold#.#.#.")
-    ///                 .padding()
-    ///
-    ///         }
-    /// }
-    /// ```
     /// - Parameters:
     ///   - message: What message should be typed on the screen.
     ///   - speed: How fast the message should be typed.
@@ -126,18 +122,6 @@ public struct TypedText: View {
     }
     
     /// Creates a "typed on" effect where each letter of the message is revealed over time, as controlled by the given message and speed arguments.
-    /// You can use `#` signs to introduce a pause as letters are typed, which can build tension or create drama.
-    /// For example:
-    /// ```swift
-    /// struct ContentView: View {
-    ///         var body: some View {
-    ///
-    ///             TypedText("Come with me###, and see what has been foretold#.#.#.")
-    ///                 .padding()
-    ///
-    ///         }
-    /// }
-    /// ```
     /// - Parameters:
     ///   - message: What message should be typed on the screen.
     ///   - speed: How fast the message should be typed.
@@ -159,18 +143,6 @@ public struct TypedText: View {
     }
     
     /// Creates a "typed on" effect where each letter of the message is revealed over time, as controlled by the given message and speed arguments.
-    /// You can use `#` signs to introduce a pause as letters are typed, which can build tension or create drama.
-    /// For example:
-    /// ```swift
-    /// struct ContentView: View {
-    ///         var body: some View {
-    ///
-    ///             TypedText("Come with me###, and see what has been foretold#.#.#.")
-    ///                 .padding()
-    ///
-    ///         }
-    /// }
-    /// ```
     /// - Parameter message: What message should be typed on the screen.
     public init(_ message: String = "Come with me###, and see what has been foretold#.#.#.") {
         
