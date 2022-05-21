d5a847d176cbf656c3469cd8b232b172b043d237
Russell Gordon <github@russellgordon.ca>
2022-05-21 12:36:30 -0400

    Refactor view modifier for simpler naming.

--- Sources/RetroText/ViewModifiers/RetroTextViewModifier.swift
+++ Sources/RetroText/ViewModifiers/RetroTextViewModifier.swift
@@ -1,5 +1,5 @@
 //
-//  RetroTextViewModifier.swift
+//  RetroFontViewModifier.swift
 //  
 //
 //  Created by Russell Gordon on 2022-05-21.
@@ -8,13 +8,18 @@
 import Foundation
 import SwiftUI
 
-public struct RetroTextViewModifier: ViewModifier {
+public struct RetroFontViewModifier: ViewModifier {
     
     // MARK: Stored properties
     let font: RetroTextFont
     let size: CGFloat
     
     // MARK: Functions
+    public init(_ font: RetroTextFont, size: CGFloat) {
+        self.font = font
+        self.size = size
+    }
+    
     public func body(content: Content) -> some View {
         
         content
@@ -28,20 +33,20 @@ public struct RetroTextViewModifier: ViewModifier {
 extension View {
     
     /// Apply a class video game-style font with the given name and size that scales with the body text style.
-    public func retroText(font: RetroTextFont = .kongText,
+    public func retroFont(_ font: RetroTextFont = .kongText,
                           size: CGFloat = 24.0) -> some View {
         
-        self.modifier(RetroTextViewModifier(font: font,
+        self.modifier(RetroFontViewModifier(font,
                                             size: size))
         
     }
-
+    
     /// Apply a class video game-style font whose size scales with the body text style.
-    public func retroText(font: RetroTextFont = .kongText) -> some View {
+    public func retroFont(_ font: RetroTextFont = .kongText) -> some View {
         
-        self.modifier(RetroTextViewModifier(font: font,
+        self.modifier(RetroFontViewModifier(font,
                                             size: 24.0))
         
     }
-
+    
 }
