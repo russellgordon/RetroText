d20ebe10caf16302f7183d192b79845b5688d50c
Russell Gordon <github@russellgordon.ca>
2022-05-27 06:48:16 -0400

    Trying to fix crash.

--- Sources/RetroText/Views/TypedText.swift
+++ Sources/RetroText/Views/TypedText.swift
@@ -68,6 +68,7 @@ public struct TypedText: View {
                     // When the message has changed under our feet...
                     if oldMessage != message {
                         resetProperties()
+                        return
                     }
                     
                     // Skip spaces
