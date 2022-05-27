d52e23ca79aaf2171e628d45a93e4d1dd45adc4b
Russell Gordon <github@russellgordon.ca>
2022-05-27 07:15:03 -0400

    Attempting to fix crash.

--- Sources/RetroText/Extensions/StringProtocol.swift
+++ Sources/RetroText/Extensions/StringProtocol.swift
@@ -12,11 +12,11 @@ extension StringProtocol {
     // Allows access to a string using it's index in subscript notation, like the gods intended
     // SEE: https://www.simpleswiftguide.com/get-character-from-string-using-its-index-in-swift/
     subscript(offset: Int) -> Character {
-        if offset < self.count {
+//        if offset < self.count {
             return self[index(startIndex, offsetBy: offset)]
-        } else {
-            return "~"
-        }
+//        } else {
+//            return "~"
+//        }
     }
     
 }
